a51a3279a3d6e71194d59669b26ae56629011022
David Whitney <david@davidwhitney.co.uk>
2013-08-07 19:48:43 +0100

    Added proxy

---
+++ ReallySimpleProxy/RequestProxying/IProxy.cs
@@ -0,0 +1,9 @@
+﻿using Nancy;
+
+namespace ReallySimpleProxy.RequestProxying
+{
+    public interface IProxy
+    {
+        dynamic ProxyRequest(NancyContext ctx);
+    }
+}
---
+++ ReallySimpleProxy/RequestProxying/Proxy.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Net;
+using Nancy;
+
+namespace ReallySimpleProxy.RequestProxying
+{
+    public class Proxy : IProxy
+    {
+        private readonly IForwardingRequestCreator _requestCreator;
+
+        public Proxy(IForwardingRequestCreator requestCreator)
+        {
+            _requestCreator = requestCreator;
+        }
+
+        public dynamic ProxyRequest(NancyContext ctx)
+        {
+            var request = _requestCreator.CloneRequest(ctx.Request);
+            var body = _requestCreator.ProcessBodyHandlers(ctx.Request);
+            
+            if (body != null)
+            {
+                var requestStream = request.GetRequestStream();
+                using (var writer = new StreamWriter(requestStream))
+                {
+                    writer.Write(body);
+                    writer.Flush();
+                }
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseStream = response.GetResponseStream();
+            return new Nancy.Responses.StreamResponse(() => responseStream, response.ContentType);
+        }
+    }
+}
